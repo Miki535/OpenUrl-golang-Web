fc8bb3c6700754718718ecd2fb45a948bc828720
Taras Shcherban <147646967+Miki535@users.noreply.github.com>
2024-02-26 19:40:40 +0200

    v1.0
    Maybe i FINISH this project

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
-package main 
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os/exec"
+	"runtime"
+)
 
-func main()  {
-	fmt.Println("Hello world!")
+var tpl = template.Must(template.ParseFiles("templates/index.html"))
+
+func main() {
+	http.HandleFunc("/", backendfunc)
+	http.ListenAndServe(":8080", nil)
+}
+
+func Open(url string) {
+	var err error
+
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("cmd", "/c", "start", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func backendfunc(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		URL := r.FormValue("URL")
+		Open(URL)
+	}
+	tpl.Execute(w, nil)
 }
